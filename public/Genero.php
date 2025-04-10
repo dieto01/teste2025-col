2b7be125d53e62c431467e36042502c3750faad4
dieto01 <diego.silva1225@etec.sp.gov.br>
2025-04-10 01:23:47 +0000

    entidade genero2

--- public/Genero.php
+++ public/Genero.php
@@ -1,14 +1,13 @@
 <?php
 
 namespace Application\Entity;
-
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\GeneratedValue;
 
-#[Entity, Table(name: 'generos')]
+#[Entity, Table(name : 'generos')]
 class Genero {
     #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
     private int $id;
